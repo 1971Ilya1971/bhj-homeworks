da4ee409008ca10e51e5c1b51d625a8dd2df3835
Илья Валерьевич Захарченко <94094566+1971Ilya1971@users.noreply.github.com>
2022-08-14 09:24:18 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,11 +3,11 @@ let cookie = document.getElementById("cookie");
 let counter = 0;
 
 cookie.onclick = function() {
-    counter++;
-    clickerCounter.textContent = counter;
-    if (counter % 2 == 0) {
-        cookie.width = 200;
+  counter++;
+  clickerCounter.textContent = counter;
+  if (counter % 2 == 0) {
+    cookie.width = 200;                  
     } else {
-        cookie.width = 180;
+      cookie.width = 180;
+      }
     }
-}
