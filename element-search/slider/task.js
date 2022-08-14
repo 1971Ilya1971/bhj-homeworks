d3b42ce5845aa06b2cacd3b834d91b49634a0ee3
Илья Валерьевич Захарченко <94094566+1971Ilya1971@users.noreply.github.com>
2022-08-14 15:41:43 +0300

    Update task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -7,59 +7,57 @@ function showSlides(direction) {
     
     activeIndex = slides.findIndex((elem) => {
       if (elem.classList.contains('slider__item_active') == true) {
-            return true
+        return true
       }
     });
     console.log(activeIndex);
     let nextIndex
     if (direction == 1) {
-        if (activeIndex == slidesCount - 1) {
+      if (activeIndex == slidesCount - 1) {
         nextIndex = 0;
         } else {
-            nextIndex = activeIndex + 1;
+          nextIndex = activeIndex + 1;
         }
         
     } else {
-        if (activeIndex == 0) {
-            nextIndex = slidesCount - 1;
+      if (activeIndex == 0) {
+        nextIndex = slidesCount - 1;
         } else {
-            nextIndex = activeIndex - 1;
+          nextIndex = activeIndex - 1;
         } 
     }
-    slides[activeIndex].classList.remove('slider__item_active');
-    slides[nextIndex].classList.add('slider__item_active');
-    dots[activeIndex].classList.remove('slider__dot_active');
-    dots[nextIndex].classList.add('slider__dot_active');
-    console.log(nextIndex);
-}
+      slides[activeIndex].classList.remove('slider__item_active');
+      slides[nextIndex].classList.add('slider__item_active');
+      dots[activeIndex].classList.remove('slider__dot_active');
+      dots[nextIndex].classList.add('slider__dot_active');
+      console.log(nextIndex);
+    }
 
-buttons.forEach((elem, index) => {
-    elem.addEventListener('click', () => {
-        showSlides(index)   
+      buttons.forEach((elem, index) => {
+        elem.addEventListener('click', () => {
+          showSlides(index)   
     })
 })
 
 dots.forEach((elem, index) => {
-    elem.addEventListener('click', () => {
-        showDot(index)   
+  elem.addEventListener('click', () => {
+    showDot(index)   
     })
 })
 
 function showDot(index) {
-    slides.forEach((elem, i) => {
-        if (index == i) {
-            elem.classList.add('slider__item_active'); 
-        } else {
-            elem.classList.remove('slider__item_active');
-        }
+  slides.forEach((elem, i) => {
+    if (index == i) {
+      elem.classList.add('slider__item_active'); 
+      } else {
+        elem.classList.remove('slider__item_active');
+      }
     })
     dots.forEach((elem, i) => {
-        if (index == i) {
-            elem.classList.add('slider__dot_active'); 
+      if (index == i) {
+        elem.classList.add('slider__dot_active'); 
         } else {
-            elem.classList.remove('slider__dot_active');
+          elem.classList.remove('slider__dot_active');
         }
     })
-
 }
-s
