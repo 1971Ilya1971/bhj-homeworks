1f5690f0ef8741bac6e5083e653797103ba46b88
Илья Валерьевич Захарченко <94094566+1971Ilya1971@users.noreply.github.com>
2022-08-14 09:31:06 +0300

    Update task.js

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,22 +2,22 @@ let dead = document.getElementById('dead');
 let lost = document.getElementById('lost');
 
 getHole = (index) => {
-    let currentHole = document.getElementById(`hole${index}`);
-    currentHole.onclick = function() {
-        if (currentHole.className.includes('hole_has-mole')) {
-            dead.textContent++;
-            if (dead.textContent >= 10) {
-                alert("Победа!");
-            }
-        } else {
+  let currentHole = document.getElementById(`hole${index}`);
+  currentHole.onclick = function() {
+    if (currentHole.className.includes('hole_has-mole')) {
+      dead.textContent++;
+      if (dead.textContent >= 10) {
+        alert("Победа!");
+        }
+          } else {
             lost.textContent++;
             if (lost.textContent >= 5) {
-                alert("Вы проиграли");
+              alert("Вы проиграли");
             }
         }
     }
 };
 
 for (let index = 1; index <= 9; index ++) {
-    getHole(index);
+  getHole(index);
 };
