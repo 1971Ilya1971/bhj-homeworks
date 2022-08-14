5cb591d248d48120a0a0e34ea165d28ea84d4346
Илья Валерьевич Захарченко <94094566+1971Ilya1971@users.noreply.github.com>
2022-08-14 16:04:05 +0300

    Update task.js

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -11,35 +11,34 @@ xhr.send();
 
 
 xhr.onload = function() {
-   if (xhr.status != 200) {
-       alert(`Ошибка ${xhr.status}: ${xhr.statusText}`)
-   } else {
-       loader.classList.remove('loader_active')
-        let itemsObj = xhr.response;
-            console.log(itemsObj.response.Valute);
-        let itemsArr = Object.values(itemsObj.response.Valute);
+  if (xhr.status != 200) {
+    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`)
+    } else {
+      loader.classList.remove('loader_active')
+      let itemsObj = xhr.response;
+      console.log(itemsObj.response.Valute);
+      let itemsArr = Object.values(itemsObj.response.Valute);
         
-            itemsArr.forEach(elem => {
-            let itemElem = document.createElement('div');
-            itemElem.className = 'item';
-            itemsContainer.append(itemElem);
+      itemsArr.forEach(elem => {
+        let itemElem = document.createElement('div');
+        itemElem.className = 'item';
+        itemsContainer.append(itemElem);
 
-            let itemCode = document.createElement('div');
-            itemCode.className = 'item__code';
-            itemElem.append(itemCode);
+         let itemCode = document.createElement('div');
+         itemCode.className = 'item__code';
+         itemElem.append(itemCode);
 
-            let itemValue = document.createElement('div');
-            itemValue.className = 'item__value';
-            itemElem.append(itemValue);
+         let itemValue = document.createElement('div');
+         itemValue.className = 'item__value';
+         itemElem.append(itemValue);
 
-            let itemCurr = document.createElement('div');
-            itemCurr.className = 'item__currency';
-            itemCurr.innerHTML = 'руб.'
-            itemElem.append(itemCurr);
+         let itemCurr = document.createElement('div');
+         itemCurr.className = 'item__currency';
+         itemCurr.innerHTML = 'руб.'
+         itemElem.append(itemCurr);
 
-            itemCode.innerHTML = elem.CharCode;
-            itemValue.innerHTML = elem.Value;
-        });
-
+         itemCode.innerHTML = elem.CharCode;
+         itemValue.innerHTML = elem.Value;
+      });
    }
 }
