362e0415bda7df5051b6c8c7853efde64d9c6a6e
Илья Валерьевич Захарченко <94094566+1971Ilya1971@users.noreply.github.com>
2022-08-14 16:05:53 +0300

    Update task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,25 +4,25 @@ const form = document.getElementById('form');
 const inputWrapperButton = document.querySelector('.input__wrapper-button');
 
 inputWrapperButton.addEventListener('click', () => {
-    progress.setAttribute('value', 0);
+  progress.setAttribute('value', 0);
 });
 
 send.addEventListener('click', () => {
-    const fileDesc = document.querySelector('.input__wrapper-desc');
+  const fileDesc = document.querySelector('.input__wrapper-desc');
 
-    if (fileDesc.textContent) {
-        upload();
-    };
+  if (fileDesc.textContent) {
+    upload();
+  };
     return false;
-});
+  });
 
 const upload = function() {
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData(form);
+  const xhr = new XMLHttpRequest();
+  const formData = new FormData(form);
 
-    xhr.upload.onprogress = (evt) => {
-        progress.setAttribute('value', evt.loaded/evt.total);
-    };
+  xhr.upload.onprogress = (evt) => {
+    progress.setAttribute('value', evt.loaded/evt.total);
+  };
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
     xhr.send(formData);
-};
+  };
