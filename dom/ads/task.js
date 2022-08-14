023c61c4a1b82bca70501e6058fc5c4b3491205a
Илья Валерьевич Захарченко <94094566+1971Ilya1971@users.noreply.github.com>
2022-08-14 15:43:11 +0300

    Update task.js

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,16 +1,16 @@
 let caseArr = Array.from(document.querySelectorAll('.rotator__case'));
 
 let rotate = function() {
-    let activeCaseIndex = caseArr.findIndex((elem, )=> {
-   return elem.classList.contains('rotator__case_active')
+  let activeCaseIndex = caseArr.findIndex((elem, )=> {
+    return elem.classList.contains('rotator__case_active')
 });
 
 caseArr[activeCaseIndex].classList.remove('rotator__case_active');
 
 if (activeCaseIndex < (caseArr.length - 1)) {
-    caseArr[activeCaseIndex + 1].classList.add('rotator__case_active');
+  caseArr[activeCaseIndex + 1].classList.add('rotator__case_active');
 } else {
-    caseArr[0].classList.add('rotator__case_active'); 
+  aseArr[0].classList.add('rotator__case_active'); 
 } 
 }
 
