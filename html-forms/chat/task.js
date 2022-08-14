97c99d3bf0a7da8068b2cb2e8431eef83afbfa5a
Илья Валерьевич Захарченко <94094566+1971Ilya1971@users.noreply.github.com>
2022-08-14 09:37:18 +0300

    Update task.js

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -3,23 +3,23 @@ let textBox = document.querySelector('.chat-widget__input');
 const messages = document.querySelector( '.chat-widget__messages' );
 let robotMessages = ['Здорова','Нет не надо','Как сам','Секс и косметику не предлагать','Ок'];
 document.addEventListener('click', () => {
-    chat.classList.add('chat-widget_active');
+  chat.classList.add('chat-widget_active');
 })
 
 document.addEventListener('keydown', e => {
-    if (e.keyCode == 13) {
-      let message = textBox.value;
-      var time = new Date();
-      var timeStr = time.toLocaleTimeString();
-      let rand = Math.floor(Math.random()*robotMessages.length);
-      let rValue = robotMessages[rand];
-      if (message != '') {
-        messages.innerHTML += `
-        <div class="message message_client">
-          <div class="message__time">${timeStr}</div>
-          <div class="message__text">
-            ${message}
-          </div>
+  if (e.keyCode == 13) {
+    let message = textBox.value;
+    var time = new Date();
+    var timeStr = time.toLocaleTimeString();
+    let rand = Math.floor(Math.random()*robotMessages.length);
+    let rValue = robotMessages[rand];
+    if (message != '') {
+      messages.innerHTML += `
+      <div class="message message_client">
+      <div class="message__time">${timeStr}</div>
+      <div class="message__text">
+      ${message}
+        </div>
         </div>
       `;
       textBox.value = '';
@@ -28,14 +28,14 @@ document.addEventListener('keydown', e => {
         <div class="message__time">${timeStr}</div>
         <div class="message__text">
           ${rValue}
-        </div>
-      </div>
-    `;
+            </div>
+            </div>
+         `;
     
 
     } else {
-    alert('Строка пуста')
-}      
-    }
+      alert('Строка пуста')
+    }      
+   }
 });
 
