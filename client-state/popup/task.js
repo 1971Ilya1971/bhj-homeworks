93cbe16a1ea8a9ab0364e5bfbd2e3eca9444cddb
Илья Валерьевич Захарченко <94094566+1971Ilya1971@users.noreply.github.com>
2022-08-14 15:58:15 +0300

    Update task.js

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,16 +1,16 @@
 const popup = document.getElementById('subscribe-modal'),
-    popupClose = document.querySelector('#subscribe-modal .modal__close');
+popupClose = document.querySelector('#subscribe-modal .modal__close');
 
 if (!getCookie('popup')) {
-    popup.classList.add('modal_active');
+  popup.classList.add('modal_active');
 }
 
 popupClose.addEventListener('click', () => {
-    document.cookie = 'popup=itwas';
-    popup.classList.remove('modal_active');
+  document.cookie = 'popup=itwas';
+  popup.classList.remove('modal_active');
 });
 
 function getCookie(name) {
-    var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+  var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
